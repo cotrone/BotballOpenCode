73e02dba24adfb1c86e3707eb4ecafb1b13ccf01
Garrett Sickles <garrettsickles@gmail.com>
2012-06-27 00:03:10 -0500

    FINALLY added servo threading

--- CBC/Servo/servolib.h
+++ CBC/Servo/servolib.h
@@ -5,32 +5,40 @@
 #include <stdio.h>
 struct servo_properties
 {
-	int port; //Servo Port
+	int port;
 	int max;
 	int min;
-	int tpm; //Ticks per Movement
-	long latency; //ms between each movement
+	int tpm;
+	long latency;
 };
 
 typedef struct servo_properties *servo;
 servo cbcservo[4];
 
-servo build_servo(int localport, int localmin, int localmax, int localtpm, long locallatency)
+int servo_port, servo_final_position, servo_inital_position, servo_tpm, servo_min, servo_max;
+long servo_latency;
+
+servo build_servo(int port, int min, int max, int tpm, long latency)
 {
-	cbcservo[localport] = malloc(sizeof(struct servo_properties));
-	cbcservo[localport]->port = localport;
-	cbcservo[localport]->tpm = localtpm;
-	cbcservo[localport]->max = localmax;
-	cbcservo[localport]->min = localmin;
-	cbcservo[localport]->latency = locallatency;
-	return(cbcservo[localport]);
+	cbcservo[port] = malloc(sizeof(struct servo_properties));
+	cbcservo[port]->port = port;
+	cbcservo[port]->tpm = tpm;
+	cbcservo[port]->max = max;
+	cbcservo[port]->min = min;
+	cbcservo[port]->latency = latency;
+	return(cbcservo[port]);
 }
-//Servo Movement
+void rebuild_servo(servo properties, int tpm, long locallatency)
+{
+    properties->tpm = tpm;
+    properties->latency = latency;
+}
 void wait_servo(servo properties, int position)
 {
-	int delta, initial,i;
-	initial = get_servo_position(properties->port);
-	delta = (position - initial) / properties->tpm;
+	int i;
+	int initial = get_servo_position(properties->port);
+	int delta = (position - initial) / properties->tpm;
+    
 	if(properties->max >= position && properties->min <= position)
 	{
 		if(initial < position)
@@ -50,5 +58,47 @@ void wait_servo(servo properties, int position)
 			}
 		}
 	}
+    if(get_servo_position(properties->port) != position)
+    {
+        set_servo_position(properties->port, position);
+    }
+}
+void move_servo(servo properties, int position)
+{
+    servo_port = properties->port;
+    servo_final_position = position;
+    servo_intitial_position = get_servo_position(properties->port);
+    servo_tpm = properties->tpm;
+    servo_latency = properties->latency;
+    start_process(servo_thread_movement);
+}
+void servo_thread_movement()
+{
+    int i;
+    int delta = (servo_final_position - servo_inital_position) / servo_tpm;
+	if(servo_max >= servo_final_position && servo_min <= servo_final_position)
+	{
+		if(servo_inital_position < servo_final_position)
+		{
+			for(i = 0; i < delta; i++)
+			{
+				set_servo_position(servo_port, servo_inital_position + (servo_tpm * i));
+				msleep(servo_latency);
+			}
+		}
+		else if(servo_inital_position > servo_final_position)
+		{
+			for(i = 0; i > delta; i--)
+			{
+				set_servo_position(servo_port, servo_inital_position + (servo_tpm * i));
+				msleep(servo_latency);
+			}
+		}
+ 
+	}
+    if(get_servo_position(servo_port) != servo_final_position)
+    {
+        set_servo_position(servo_port, servo_final_position);
+    }
 }
 #endif
