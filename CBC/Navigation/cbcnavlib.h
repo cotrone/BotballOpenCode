301df7a62093026617da5a76ee593aa762bdb6bf
Garrett Sickles <garrettsickles@gmail.com>
2012-06-26 23:13:10 -0500

    Creates New Drivetrain Declaration

--- CBC/Navigation/cbcnavlib.h
+++ CBC/Navigation/cbcnavlib.h
@@ -3,52 +3,103 @@
 
 #define PI 3.14159
 
-struct CBC_robot
-{
-	float drivetrain;
-	
-	struct wheel
-	{
-		int port;
-		long ticks_cycle;
-		float speed;
-		float wheel_diameter;
-	} lwheel, rwheel;
-} cbcrobot;
+void build_lwheel(int port, long ticks_cycle, float speed_proportion, float wheel_diameter, float radial_distance);
+// 0 <--> 3 (port), ~1100 (ticks), 1.0 + | - 0.25 (unitless), + in mm, + in mm
+//Shortcut to build the left wheel structure to be utilized by various functions
 
-void wait_for_cbc()
+void build_rwheel(int port, long ticks_cycle, float speed_proportion, float wheel_diameter, float radial_distance);
+// 0 <--> 3 (port), ~1100 (ticks), 1.0 + | - 0.25 (unitless), + in mm, + in mm
+//Shortcut to build the right wheel structure to be utilized by various functions
+
+int cbc_straight(int speed, float distance);
+// 0 <--> 1000, + | - in mm
+//Moves the center of the CBC drivetrain in a straight line
+//Returns 1 if executed, -1 if error is detected
+
+int cbc_arc(int speed, float radius, float theta)
+// 0 <--> 1000 (unitless), + | - in mm, + | - in degrees
+//Moves the center of the CBC drivetrain in a constant radial arc
+//Returns 1 if executed, -1 if error is detected
+
+int cbc_spin(int speed, float theta);
+// 0 <--> 1000, + | - in degrees
+//Rotates the center of the CBC drivetrain 
+//Returns 1 if executed, -1 if error is detected
+
+struct wheel
 {
-    bmd(cbcrobot.rwheel.port);
-    bmd(cbcrobot.lwheel.port);
-}
-void cbc_straight(int speed, float distance)
+    int port;
+    long ticks_cycle;
+    float speed_proportion;
+    float wheel_diameter;
+    float radial_distance;
+} lwheel, rwheel;
+
+void build_lwheel(int port, long ticks_cycle, float speed_proportion, float wheel_diameter, float radial_distance)
 {
-	float lticks = distance * cbcrobot.lwheel.ticks_cycle / (cbcrobot.lwheel.wheel_diameter * PI);
-	float rticks = distance * cbcrobot.rwheel.ticks_cycle / (cbcrobot.rwheel.wheel_diameter * PI);
-	
-    mrp(cbcrobot.lwheel.port, (int)((float)speed * cbcrobot.lwheel.speed), (long)lticks);
-	mrp(cbcrobot.rwheel.port, (int)((float)speed * cbcrobot.rwheel.speed), (long)rticks);
+    lwheel.port = port;
+    lwheel.ticks_cycle = ticks_cycle;
+    lwheel.speed_proportion = speed_proportion;
+    lwheel.wheel_diameter = wheel_diameter;
+    lwheel.radial_distance = radial_distance;
 }
-void cbc_arc(int speed, float radius, float theta) // 0 <--> 1000 (unitless), + | - in mm, + | - in degrees
+void build_rwheel(int port, long ticks_cycle, float speed_proportion, float wheel_diameter, float radial_distance)
 {
-	float arc_length = radius * theta * PI / 180.0;
-	float ldistance = (radius - (cbcrobot.drivetrain * 0.5)) * theta * (PI / 180.0);
-	float rdistance = (radius + (cbcrobot.drivetrain * 0.5)) * theta * (PI / 180.0);
-	float lticks = ldistance * cbcrobot.lwheel.ticks_cycle / (cbcrobot.lwheel.wheel_diameter * PI);
-	float rticks = rdistance * cbcrobot.rwheel.ticks_cycle / (cbcrobot.rwheel.wheel_diameter * PI);
-	
-	mrp(cbcrobot.lwheel.port, (int)((float)speed * cbcrobot.lwheel.speed * (ldistance / arc_length)), (long)lticks);
-	mrp(cbcrobot.rwheel.port, (int)((float)speed * cbcrobot.rwheel.speed * (rdistance / arc_length)), (long)rticks);
+    rwheel.port = port;
+    rwheel.ticks_cycle = ticks_cycle;
+    rwheel.speed_proportion = speed_proportion;
+    rwheel.wheel_diameter = wheel_diameter;
+    rwheel.radial_distance = radial_distance;
 }
-void cbc_spin(int speed, float theta)
+int wait_movement()
 {
-	float rdistance = cbcrobot.drivetrain * 0.5 * theta * PI / 180.0;
-	float ldistance = rdistance * -1.0;
-	float lticks = ldistance * cbcrobot.lwheel.ticks_cycle / (cbcrobot.lwheel.wheel_diameter * PI);
-	float rticks = rdistance * cbcrobot.rwheel.ticks_cycle / (cbcrobot.rwheel.wheel_diameter * PI);
-	
-	mrp(cbcrobot.lwheel.port, (int)((float)speed * cbcrobot.lwheel.speed), (long)lticks);
-	mrp(cbcrobot.rwheel.port, (int)((float)speed * cbcrobot.rwheel.speed), (long)rticks);
+    bmd(rwheel.port);
+    bmd(lwheel.port);
+    return 1;
 }
-	
+int cbc_straight(int speed, float distance)
+{
+	float lticks = (distance * lwheel.ticks_cycle) / (lwheel.wheel_diameter * PI);
+	float rticks = (distance * rwheel.ticks_cycle) / (rwheel.wheel_diameter * PI);
+    float lspeed = (float)speed * lwheel.speed_proportion * ldistance / arc_length;
+    float rspeed = (float)speed * rwheel.speed_proportion * rdistance / arc_length;
+    
+    if(rspeed > 1000 || lspeed > 1000 || rspeed < 0 || lspeed < 0)
+    {
+        printf("\nWarning! Invalid Speed\n")
+        return -1;
+    }
+    else
+    {
+        mrp(lwheel.port, (int)lspeed, (long)lticks);
+        mrp(rwheel.port, (int)rspeed, (long)rticks);
+        return 1;
+    }
+}
+int cbc_arc(int speed, float radius, float theta) // 0 <--> 1000 (unitless), + | - in mm, + | - in degrees
+{
+	float arc_length = radius * theta * (PI / 180.0);
+	float ldistance = (radius - lwheel.radial_distance) * theta * (PI / 180.0);
+	float rdistance = (radius + rwheel.radial_distance) * theta * (PI / 180.0);
+	float lticks = (ldistance * lwheel.ticks_cycle) / (lwheel.wheel_diameter * PI);
+	float rticks = (rdistance * rwheel.ticks_cycle) / (rwheel.wheel_diameter * PI);
+    float lspeed = (float)speed * lwheel.speed_proportion * ldistance / arc_length;
+    float rspeed = (float)speed * rwheel.speed_proportion * rdistance / arc_length;
+    
+    if(rspeed > 1000 || lspeed > 1000 || rspeed < 0 || lspeed < 0)
+    {
+        printf("\nWarning! Invalid Speed\n")
+        return -1;
+    }
+    else
+    {
+        mrp(lwheel.port, (int)lspeed, (long)lticks);
+        mrp(rwheel.port, (int)rspeed, (long)rticks);
+        return 1;
+    }
+}
+int cbc_spin(int speed, float theta)
+{
+	return(cbc_arc(speed, 0.0, theta));
+}
 #endif
