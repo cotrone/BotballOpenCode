3f809ec366f40edffa6c67b843c7e159dd1829ac
garrettsickles <garrettsickles@gmail.com>
2012-07-13 06:02:01 -0500

    Fixed Servolib.h

--- opencode/cbc/servo/servolib.h
+++ opencode/cbc/servo/servolib.h
@@ -1,37 +1,35 @@
 #ifndef __SERVOLIB_H__
 #define __SERVOLIB_H__
-
 #include <pthread.h>
-
 struct servo_properties
 {
-	int port;
-	int max;
-	int min;
-	int is_moving;
-    int connect_mask;
+	int port, max, min, initial, delta;
 	pthread_t process_id;
-	long next_latency;
-    struct servo_movement *next_movement;
+    struct servo_movement *next_move;
 }cbcservo[4];
-
 struct servo_movement
 {
-	int position;
-	int tpm;
+	int position, tpm;
 	long latency;
 };
-
 typedef struct servo_movement *servo_movement;
 typedef struct servo_properties *servo;
 pthread_mutex_t servo_mem = PTHREAD_MUTEX_INITIALIZER;
-
 servo build_servo(int port, int min, int max)
 {
 	cbcservo[port].port = port;
 	cbcservo[port].min = min;
 	cbcservo[port].max = max;
-	return(&cbcservo[port]);
+	if(cbcservo[port].min >=0 && cbcservo[port].max <= 2048 && cbcservo[port].min < cbcservo[port].max)
+	{
+		return(&cbcservo[port]);
+	}
+	else
+	{
+		cbcservo[port].min = 0;
+		cbcservo[port].max = 2048;
+		return(&cbcservo[port]);
+	}
 }
 servo_movement build_servo_movement(int position, int tpm, long latency)
 {
@@ -44,82 +42,46 @@ servo_movement build_servo_movement(int position, int tpm, long latency)
 void *control_servo(void *ptr_servo)
 {
 	pthread_mutex_lock(&servo_mem);
+    int i;
 	servo build_properties = (struct servo_properties *) ptr_servo;
-	int i, delta;
-	int initial = get_servo_position(build_properties->port);
-    delta = (build_properties->next_position - initial) / build_properties->next_tpm;
-	if(initial == -1)
-	{
-		set_servo_position(build_properties->port, build_properties->next_position);
-	}
-	else if(build_properties->max >= build_properties->next_position && build_properties->min <= build_properties->next_position && build_properties->min < build_properties->max)
-	{
-		if(initial < build_properties->next_position)
+		if(build_properties->delta > 0)
 		{
-			for(i = 0; i < delta; i++)
+			for(i = 0; i < build_properties->delta; i++)
 			{
-				set_servo_position(build_properties->port, initial + (build_properties->next_tpm * i));
-				msleep(build_properties->next_latency);
+				set_servo_position(build_properties->port, build_properties->initial + (build_properties->next_move->tpm * i));
+				msleep(build_properties->next_move->latency);
 			}
 		}
-		if(initial > build_properties->next_position)
+		if(build_properties->delta < 0)
 		{
-			for(i = 0; i > delta; i--)
+			for(i = 0; i > build_properties->delta; i--)
 			{
-				set_servo_position(build_properties->port, initial + (build_properties->next_tpm * i));
-				msleep(build_properties->next_latency);
+				set_servo_position(build_properties->port, build_properties->initial + (build_properties->next_move->tpm * i));
+				msleep(build_properties->next_move->latency);
 			}
 		}
-		if(get_servo_position(build_properties->port) != build_properties->next_position)
+		if(get_servo_position(build_properties->port) != build_properties->next_move->position)
 		{
-			set_servo_position(build_properties->port, build_properties->next_position);
+			set_servo_position(build_properties->port, build_properties->next_move->position);
 		}
-	}
-	else
-	{
-		printf("Invalid Movement of Servo: %d", build_properties->port);
-	}
-	build_properties->is_moving = 0;
 	pthread_mutex_unlock(&servo_mem);
 }
 void move_servo(servo build_properties,servo_movement move_properties)
 {
-	int thread_num, i;
-	pthread_t this_thread;
-	build_properties->is_moving = 1;
-	build_properties->next_position = move_properties->position;
-	build_properties->next_tpm = move_properties->tpm;
-	build_properties->next_latency = move_properties->latency;
-    if(build_properties->connect_mask & (1 << build_properties->port) == (1 << build_properties->port))
-    {
-        if((thread_num = pthread_create(&this_thread, NULL, &control_servo, (void *)build_properties)))
-        {
-            printf("Threading Failure: %d\n", thread_num);
-        }
-        else
-        {
-            build_properties->process_id = this_thread;
-        }
+    if(build_properties->initial == -1)
+	{
+		set_servo_position(build_properties->port, build_properties->next_move->position);
+	}
+    else if(build_properties->max >= build_properties->next_move->position && build_properties->min <= build_properties->next_move->position)
+	{
+    build_properties->next_move = move_properties;
+    build_properties->delta = (build_properties->next_move->position - get_servo_position(build_properties->port)) / build_properties->next_move->tpm;
+    pthread_create(&build_properties->process_id, NULL, &control_servo, (void *)build_properties);
     }
-    else
-    {
-        for(i = 0; i <= 3; i++)
-        {
-            if(build_properties->connect_mask & (1 << i) == (1 << i))
-            {
-                get_servo_position(i)
-                if((thread_num = pthread_create(&this_thread, NULL, &control_servo, (void *)build_properties)))
-                {
-                    printf("Threading Failure: %d\n", thread_num);
-                }
-                else
-                {
-                    build_properties->process_id = this_thread;
-                }
-
-            }
-        }
-    }
+	    else
+	{
+		printf("Invalid Movement of Servo: %d", build_properties->port);
+	}
 }
 void bsd(servo build_properties)
 {
@@ -127,20 +89,12 @@ void bsd(servo build_properties)
 }
 void wait_servo(servo build_properties, servo_movement move_properties)
 {
-	build_properties->is_moving = 1;
-	build_properties->next_movement = move_properties;
+	build_properties->next_move = move_properties;
     move_servo(build_properties, move_properties);
     bsd(build_properties);
 }
-void connect_servos(servo default_servo, servo_movement default_movemnet, servo other_servo, servo_movement other_movemnet)
+void kill_servo(servo this_servo)
 {
-    default
-}
-void kill_servos()
-{
-	pthread_kill(cbcservo[0].process_id, 1);
-	pthread_kill(cbcservo[1].process_id, 1);
-	pthread_kill(cbcservo[2].process_id, 1);
-	pthread_kill(cbcservo[3].process_id, 1);
+	pthread_kill(this_servo->process_id, 1);
 }
 #endif
