6ca3ec3763070797d48581edd22f5f4e93d2d410
garrettsickles <garrettsickles@gmail.com>
2012-07-13 03:21:30 -0500

    Optimizing. WARNING!

---
+++ opencode/cbc/motorlib.h
@@ -0,0 +1,14 @@
+//
+//  motorlib.h
+//  BotballOpenCodeProject
+//
+//  Created by Donald Sickles on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef BotballOpenCodeProject_motorlib_h
+#define BotballOpenCodeProject_motorlib_h
+
+
+
+#endif
